067b9769ae874b3d9416987c81a4700a095c17ce
Dingping Zhang <dingpingzhang@outlook.com>
2017-08-29 00:39:40 +0800

    Update demo & add converter
    1. Update settings demo.
    2. Add StringToImageConverter converter for generating default head image.

---
+++ Source/Accelerider.Windows/Converters/StringToImageConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Accelerider.Windows.Converters
+{
+    public class StringToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var firstLetter = value.ToString().First().ToString();
+            var temp = new DrawingVisual();
+            var c = temp.RenderOpen();
+
+            var typeface = new Typeface( new FontFamily("Microsoft YaHei"), FontStyles.Normal, FontWeights.Normal, FontStretches.Normal);
+            c.DrawRectangle(Brushes.BlueViolet, null, new Rect(new Size(32, 32)));
+            c.DrawText(new FormattedText(firstLetter, culture, FlowDirection.LeftToRight, typeface, 16, Brushes.White), new Point(8, 5));
+            c.Close();
+            var image = new DrawingImage(temp.Drawing);
+            return image;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
