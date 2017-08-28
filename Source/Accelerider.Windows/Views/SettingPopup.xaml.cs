41c27b447ae50c9da09effcf681495fd1720f3aa
Dingping Zhang <dingpingzhang@outlook.com>
2017-08-28 08:41:35 +0800

    Add TabControl style, SettingsPopup, AboutDialog (Empty).

---
+++ Source/Accelerider.Windows/Views/SettingPopup.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Accelerider.Windows.Views
+{
+    /// <summary>
+    /// Interaction logic for SettingsPopup.xaml
+    /// </summary>
+    public partial class SettingsPopup
+    {
+        public SettingsPopup()
+        {
+            InitializeComponent();
+        }
+    }
+}
