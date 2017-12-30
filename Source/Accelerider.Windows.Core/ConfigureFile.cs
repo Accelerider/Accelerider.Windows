a4f36aac5d331a1eaaa89187ab28b53ce8106e47
Dingping Zhang <DingpingZhang@outlook.com>
2017-12-30 17:04:55 +0800

    Add IConfigureFile interface and its implementation
    Replace IConfigureInfo to ICongureFile

---
+++ Source/Accelerider.Windows.Core/ConfigureFile.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Linq;
+using Accelerider.Windows.Infrastructure.Interfaces;
+using Newtonsoft.Json.Linq;
+
+namespace Accelerider.Windows.Core
+{
+    public class ConfigureFile : IConfigureFile
+    {
+        private JObject _storage;
+        private string _filePath = Path.Combine(Directory.GetCurrentDirectory(), "config.json");
+
+        public ConfigureFile() => Load();
+
+        public string this[string key]
+        {
+            get => _storage.Value<string>(key);
+            set => _storage[key] = value;
+        }
+
+        public bool Contains(string key) => _storage.Values().Any(token => token.Path == key);
+
+        public T GetValue<T>(string key) => _storage.Value<T>(key);
+
+        public void Delete()
+        {
+            File.WriteAllText(_filePath, string.Empty);
+            File.Delete(_filePath);
+        }
+
+        public void Load(string filePath = null)
+        {
+            if (!string.IsNullOrEmpty(filePath)) _filePath = filePath;
+
+            if (!File.Exists(_filePath))
+            {
+                _storage = new JObject();
+                Save();
+            }
+            _storage = JObject.Parse(File.ReadAllText(_filePath));
+        }
+
+        public void Save() => File.WriteAllText(_filePath, _storage.ToString());
+    }
+}
---
+++ Source/Accelerider.Windows.Infrastructure/Interfaces/IConfigureFile.cs
@@ -0,0 +1,17 @@
+﻿namespace Accelerider.Windows.Infrastructure.Interfaces
+{
+    public interface IConfigureFile
+    {
+        string this[string key] { get; set; }
+
+        bool Contains(string key);
+
+        T GetValue<T>(string key);
+
+        void Delete();
+
+        void Load(string filePath = null);
+
+        void Save();
+    }
+}
