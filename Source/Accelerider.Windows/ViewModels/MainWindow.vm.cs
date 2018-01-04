8a9c8512c77e397f043a8eee0e8ff5aabb45a1b5
Dingping Zhang <DingpingZhang@outlook.com>
2018-01-04 21:21:09 +0800

    Rename Right... to Left..., initial commit for AppShtore

--- Source/Accelerider.Windows/ViewModels/MainWindow.vm.cs
+++ Source/Accelerider.Windows/ViewModels/MainWindow.vm.cs
@@ -1,8 +1,12 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Windows.Input;
 using Accelerider.Windows.Infrastructure;
 using Accelerider.Windows.Infrastructure.Commands;
 using Accelerider.Windows.Resources.I18N;
+using Accelerider.Windows.Views.AppStore;
 using Microsoft.Practices.Unity;
 using Prism.Regions;
 
@@ -11,6 +15,7 @@ namespace Accelerider.Windows.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         private ICommand _feedbackCommand;
+        private bool _appStoreIsDisplayed;
 
 
         public MainWindowViewModel(IUnityContainer container, IRegionManager regionManager) : base(container)
@@ -29,9 +34,41 @@ namespace Accelerider.Windows.ViewModels
             set => SetProperty(ref _feedbackCommand, value);
         }
 
+        public bool AppStoreIsDisplayed
+        {
+            get => _appStoreIsDisplayed;
+            set
+            {
+                if (_appStoreIsDisplayed) return;
+                if (!SetProperty(ref _appStoreIsDisplayed, value)) return;
+
+                var region = RegionManager.Regions[RegionNames.MainTabRegion];
+                foreach (var activeView in region.ActiveViews)
+                {
+                    region.Deactivate(activeView);
+                }
+            }
+        }
+
+        public override void OnLoaded(object view)
+        {
+            var region = RegionManager.Regions[RegionNames.MainTabRegion];
+            region.ActiveViews.CollectionChanged += OnActiveViewsChanged;
+        }
+
+        private void OnActiveViewsChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action != NotifyCollectionChangedAction.Add) return;
+
+            _appStoreIsDisplayed = false;
+            RaisePropertyChanged(nameof(AppStoreIsDisplayed));
+        }
+
         public override void OnUnloaded(object view)
         {
             AcceleriderUser.OnExit();
         }
+
+        
     }
 }
