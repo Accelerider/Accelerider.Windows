e54f3ed91d8f400e668b85248bd3f77b880172fc
Dingping Zhang <DingpingZhang@outlook.com>
2018-01-05 16:05:31 +0800

    Add Accelerider.Windows.Modules.NetDisk.Plus project for separating the functions associated Baidu.

---
+++ Source/Accelerider.Windows.Modules.NetDisk.Plus/NetDiskPlusModule.cs
@@ -0,0 +1,20 @@
+﻿using Accelerider.Windows.Infrastructure;
+using Microsoft.Practices.Unity;
+using Prism.Regions;
+
+namespace Accelerider.Windows.Modules.NetDisk.Plus
+{
+    public class NetDiskModule : ModuleBase
+    {
+        private readonly IRegionManager _regionManager;
+
+        public NetDiskModule(IUnityContainer container, IRegionManager regionManager) : base(container)
+        {
+            _regionManager = regionManager;
+        }
+
+        public override void Initialize()
+        {
+        }
+    }
+}
