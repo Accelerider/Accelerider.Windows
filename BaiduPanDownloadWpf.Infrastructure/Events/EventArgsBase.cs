7c35b63dd0159438dbec6d179c2355ca3b12ae0d
Dingping Zhang <DingpingZhang@outlook.com>
2018-04-05 14:47:12 +0800

    Delete the old version (v1).

--- BaiduPanDownloadWpf.Infrastructure/Events/EventArgsBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace BaiduPanDownloadWpf.Infrastructure.Events
-{
-    public class EventArgsBase : EventArgs
-    {
-        public DateTime Timestamp { get; } = DateTime.Now;
-    }
-}
