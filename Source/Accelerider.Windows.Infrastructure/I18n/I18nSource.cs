2056bce0f035f6bd6ed2dd8ac7c97d58a2206485
Dingping Zhang <DingpingZhang@outlook.com>
2018-08-18 19:52:54 +0800

    Add support for the i18n

---
+++ Source/Accelerider.Windows.Infrastructure/I18n/I18nSource.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace Accelerider.Windows.Infrastructure.I18n
+{
+    public class I18nSource : INotifyPropertyChanged
+    {
+        private readonly ComponentResourceKey _key;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public I18nSource(FrameworkElement element, ComponentResourceKey key)
+        {
+            _key = key;
+            element.Loaded += OnLoaded;
+            element.Unloaded += OnUnloaded;
+        }
+
+        public object Value => LanguageManager.Instance.Translate(_key);
+
+        private void OnCurrentUICultureChanged(object sender, EventArgs e)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            LanguageManager.Instance.CurrentUICultureChanged += OnCurrentUICultureChanged;
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            LanguageManager.Instance.CurrentUICultureChanged -= OnCurrentUICultureChanged;
+        }
+    }
+}
---
+++ Source/Accelerider.Windows.Infrastructure/I18n/LanguageManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Threading;
+using System.Windows;
+
+namespace Accelerider.Windows.Infrastructure.I18n
+{
+    public class LanguageManager
+    {
+        private readonly ConcurrentDictionary<string, ResourceManager> _resourceManagerStorage = new ConcurrentDictionary<string, ResourceManager>();
+
+        public event EventHandler CurrentUICultureChanged;
+
+        public static LanguageManager Instance { get; } = new LanguageManager();
+
+        private LanguageManager() { }
+
+        public CultureInfo CurrentUICulture
+        {
+            get => Thread.CurrentThread.CurrentUICulture;
+            set
+            {
+                if (EqualityComparer<CultureInfo>.Default.Equals(value, Thread.CurrentThread.CurrentUICulture)) return;
+
+                Thread.CurrentThread.CurrentUICulture = value;
+                Thread.CurrentThread.CurrentCulture = value;
+                OnCurrentUICultureChanged();
+            }
+        }
+
+        public IEnumerable<CultureInfo> AvailableCultureInfos => new[]
+        {
+            new CultureInfo("zh-CN"),
+            new CultureInfo("en-US")
+        };
+
+        public void AddResoureManager(ResourceManager resourceManager)
+        {
+            if (_resourceManagerStorage.ContainsKey(Assembly.GetCallingAssembly().FullName))
+                throw new ArgumentException("", nameof(resourceManager));
+
+            _resourceManagerStorage[Assembly.GetCallingAssembly().FullName] = resourceManager;
+        }
+
+        private void OnCurrentUICultureChanged() => CurrentUICultureChanged?.Invoke(this, EventArgs.Empty);
+
+        public object Translate(ComponentResourceKey key)
+        {
+            return GetCurrentResoureManager(key.Assembly)?.GetString(key.ResourceId.ToString(), CurrentUICulture)
+                   ?? $"<MISSING: {key}>";
+        }
+
+        private ResourceManager GetCurrentResoureManager(Assembly assembly)
+        {
+            return _resourceManagerStorage.TryGetValue(assembly.FullName, out var value) ? value : null;
+        }
+    }
+}
