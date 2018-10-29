116be825a1006d4c7a3b87aa690f0646beefa4c9
Dingping Zhang <dingpingzhang@outlook.com>
2018-10-29 19:12:05 +0800

    [Refactor] Create the Accelerider.Windows.Controls project.

---
+++ Source/Accelerider.Windows.Controls/Wizard/WizardablePanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace Accelerider.Windows.Controls.Wizard
+{
+    [ContentProperty(nameof(WizardViews))]
+    public class WizardablePanel : UserControl
+    {
+        //static WizardablePanel()
+        //{
+        //    DefaultStyleKeyProperty.OverrideMetadata(typeof(WizardablePanel), new FrameworkPropertyMetadata(typeof(WizardablePanel)));
+        //}
+
+        public static readonly DependencyProperty WizardViewsProperty = DependencyProperty.Register(
+            "WizardViews", typeof(WizardableViewCollection), typeof(WizardablePanel), new PropertyMetadata(null));
+
+        public WizardablePanel()
+        {
+            WizardViews = new WizardableViewCollection(this);
+            Loaded += (sender, e) => Content = WizardViews?.FirstOrDefault();
+        }
+
+        public WizardableViewCollection WizardViews
+        {
+            get { return (WizardableViewCollection)GetValue(WizardViewsProperty); }
+            set { SetValue(WizardViewsProperty, value); }
+        }
+    }
+}
