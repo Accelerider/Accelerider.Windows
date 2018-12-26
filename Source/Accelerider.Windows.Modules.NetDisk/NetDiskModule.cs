64c26d35fee7f42979b7460dfee694a0091cd3bc
DingpingZhang <DingpingZhang@outlook.com>
2018-12-26 09:14:01 +0800

    Delete the INetDiskApi.cs file.

--- Source/Accelerider.Windows.Modules.NetDisk/NetDiskModule.cs
+++ Source/Accelerider.Windows.Modules.NetDisk/NetDiskModule.cs
@@ -1,8 +1,5 @@
-﻿using System.Threading.Tasks;
-using Accelerider.Windows.Infrastructure;
+﻿using Accelerider.Windows.Infrastructure;
 using Accelerider.Windows.Infrastructure.Modularity;
-using Accelerider.Windows.Modules.NetDisk.Constants;
-using Accelerider.Windows.Modules.NetDisk.Models;
 using Accelerider.Windows.Modules.NetDisk.Views;
 using Accelerider.Windows.Modules.NetDisk.Views.FileBrowser;
 using Accelerider.Windows.Modules.NetDisk.Views.Transportation;
@@ -10,7 +7,6 @@ using Accelerider.Windows.TransferService;
 using Prism.Ioc;
 using Unity;
 using Prism.Regions;
-using Refit;
 
 namespace Accelerider.Windows.Modules.NetDisk
 {
@@ -26,21 +22,12 @@ namespace Accelerider.Windows.Modules.NetDisk
         public override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             // Register for container
-            containerRegistry.RegisterInstance(GetNetDiskApi());
             containerRegistry.RegisterInstance(FileTransferService.GetDownloaderManager("net-disk"));
 
             // Register for region
             _regionManager.RegisterViewWithRegion(RegionNames.MainTabRegion, typeof(FileBrowserComponent));
             _regionManager.RegisterViewWithRegion(RegionNames.MainTabRegion, typeof(TransportationComponent));
             _regionManager.RegisterViewWithRegion(RegionNames.SettingsTabRegion, typeof(TaskSettingsTabItem));
-        }
-
-        private INetDiskApi GetNetDiskApi()
-        {
-            return RestService.For<INetDiskApi>(Hyperlinks.ApiBaseAddress, new RefitSettings
-            {
-                AuthorizationHeaderValueGetter = () => Task.FromResult(Container.Resolve<IAcceleriderUser>().Token)
-            });
         }
     }
 }
