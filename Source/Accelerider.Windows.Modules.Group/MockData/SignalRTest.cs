9b1dc611cbbbc5cf0498aabd9b5a74b8ba2d1bc7
Dingping Zhang <DingpingZhang@outlook.com>
2019-01-03 23:08:37 +0800

    [Refactor] Optimized the reference of projects.

--- Source/Accelerider.Windows.Modules.Group/MockData/SignalRTest.cs
+++ Source/Accelerider.Windows.Modules.Group/MockData/SignalRTest.cs
@@ -1,19 +1,7 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.SignalR.Client;
-
+﻿
 namespace Accelerider.Windows.Modules.Group.MockData
 {
     public class SignalRTest
     {
-        public void TestMethod()
-        {
-            var connection = new HubConnectionBuilder()
-                .WithUrl("http://localhost:2333/groups",
-                    options =>
-                    {
-                        options.AccessTokenProvider = () => Task.FromResult("753951");
-                    }).Build();
-
-        }
     }
 }
