4a53ffdea4c961b8a7e85d707fc245ca8b39352b
Dingping Zhang <DingpingZhang@outlook.com>
2019-01-04 04:26:41 +0800

    Add the Package project.

---
+++ Source/Package/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Package
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var path = @"C:\Users\Dingp\source\repos\Accelerider\Source\Build\Release";
+
+            var fileTags = Directory
+                .GetFiles(path)
+                .Select(item => Path.GetFileName(item))
+                .Select(item => $"<File Name=\"{item}\" />");
+
+            var temp = string.Join(Environment.NewLine, fileTags);
+        }
+    }
+}
